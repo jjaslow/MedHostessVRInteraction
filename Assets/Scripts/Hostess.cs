35e8b4580037f4e110d04d95ef0aa0f65093a902
Jason Jaslow <jason@jaslow.com>
2020-07-16 08:27:44 -0400

    initial setup

---
+++ Assets/Scripts/Hostess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hostess : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+
+
+
+
+    public void StartTalking()
+    {
+        Debug.Log("talking");
+        transform.LookAt(player.transform.position);
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y , 0);
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource audioSource;
+
+    [SerializeField]
+    AudioClip[] clips;
+
+    bool canPlayAudio = true;
+
+    void Start()
+    {
+        StartCoroutine(PlayAudio());
+    }
+
+
+
+    IEnumerator PlayAudio()
+    {
+        while(canPlayAudio)
+        {
+            Debug.Log("audio start");
+            yield return new WaitForSeconds(5f);
+
+            audioSource.clip = clips[0];
+            audioSource.Play();
+
+            yield return new WaitForSeconds(10f);
+
+            audioSource.clip = clips[1];
+            audioSource.Play();
+        }
+    }
+
+    public void OnPressContinue()
+    {
+        canPlayAudio = false;
+        audioSource.Stop();
+        SceneManager.LoadScene("Main");
+    }
+}
