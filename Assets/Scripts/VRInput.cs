9a8d96fa55e4386a07b593c5d00fc5369f06e10f
Jason Jaslow <jason@jaslow.com>
2020-07-16 09:40:13 -0400

    x

--- Assets/Scripts/VRInput.cs
+++ Assets/Scripts/VRInput.cs
@@ -7,30 +7,30 @@ using UnityEngine.UI;
 public class VRInput : BaseInput
 {
     public Camera eventCamera = null;
-    //public OVRInput.Button clickButton = OVRInput.Button.PrimaryIndexTrigger;
-    //public OVRInput.Controller controller = OVRInput.Controller.All;
+    public OVRInput.Button clickButton = OVRInput.Button.PrimaryIndexTrigger;
+    public OVRInput.Controller controller = OVRInput.Controller.All;
 
 
-    //protected override void Awake()
-    //{
-    //    GetComponent<BaseInputModule>().inputOverride = this;
-    //}
+    protected override void Awake()
+    {
+        GetComponent<BaseInputModule>().inputOverride = this;
+    }
 
 
-    //public override bool GetMouseButton(int button)
-    //{
-    //    return OVRInput.Get(clickButton, controller);
-    //}
+    public override bool GetMouseButton(int button)
+    {
+        return OVRInput.Get(clickButton, controller);
+    }
 
-    //public override bool GetMouseButtonDown(int button)
-    //{
-    //    return OVRInput.GetDown(clickButton, controller);
-    //}
+    public override bool GetMouseButtonDown(int button)
+    {
+        return OVRInput.GetDown(clickButton, controller);
+    }
 
-    //public override bool GetMouseButtonUp(int button)
-    //{
-    //    return OVRInput.GetUp(clickButton, controller);
-    //}
+    public override bool GetMouseButtonUp(int button)
+    {
+        return OVRInput.GetUp(clickButton, controller);
+    }
 
     public override Vector2 mousePosition
     {
